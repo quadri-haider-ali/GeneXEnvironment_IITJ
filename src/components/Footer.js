4342bbf84dc824a3bfd8611ee2a1d5c2f1408b78
Haider Ali Quadri <quadri.2@iitj.ac.in>
2022-10-25 02:33:17 +0530

    final project commit

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Footer.css'
+
+const Footer = () => {
+  return (
+    <React.Fragment>
+        <div className='footer'>
+            Copyright
+        </div>
+    </React.Fragment>
+  )
+}
+
+export default Footer
